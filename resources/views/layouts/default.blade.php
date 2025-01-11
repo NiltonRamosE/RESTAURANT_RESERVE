e2238d27f53bb0b74107db3969e597395f3c144e
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-01-11 17:48:21 -0500

    fix: route index for yield

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,33 @@
-<div class="relative flex flex-col min-h-screen overflow-hidden">
-  <div class="relative z-10 flex flex-col min-h-screen">
-     @include('partials.navbar')
-      <main class="container mx-auto px-6 flex-grow pt-14 pb-8">
-        @yield('content')        
-      </main>
-    @include('partials.footer')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+  <title>@yield('title')</title>
+  <meta content="Página no original del restaurante 7 SOPAS, es un proyecto personal para simular las reservas en la entidad." property="og:description" />
+  <meta content="Página no original del restaurante 7 SOPAS, es un proyecto personal para simular las reservas en la entidad." name="description" />
+  <meta content="website" property="og:type" />
+  <meta content="7 SOPAS" property="og:site_name" />
+  <meta content="es_ES" property="og:locale" />
+  <meta
+    key="viewport"
+    content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+    name="viewport"
+  />
+  <link href="favicon.ico" rel="icon" />
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <div class="relative flex flex-col min-h-screen overflow-hidden">
+    <div class="relative z-10 flex flex-col min-h-screen">
+      @include('partials.navbar')
+        <main class="container mx-auto px-6 flex-grow pt-14 pb-8">
+          @yield('content')
+        </main>
+      @include('partials.footer')
+    </div>
   </div>
-</div>
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.default')
+
+@section('title', '7 SOPAS')
+
+@section('content')
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem perspiciatis at repudiandae id molestias dignissimos quia omnis. Ex, animi, non eum a sint debitis, voluptatum ratione consequuntur dolore suscipit enim.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aperiam accusamus iusto nemo omnis sapiente obcaecati a mollitia, architecto aspernatur, delectus quis exercitationem deleniti corrupti esse perferendis laboriosam officiis quas.
+        Quis aliquid vero porro. Eligendi magnam rerum labore quaerat ipsa quod culpa nesciunt voluptas quos quam ad excepturi nobis, dicta libero, sequi blanditiis rem mollitia debitis velit voluptatem! Qui, nesciunt!
+        Veniam, autem nemo. Ad necessitatibus voluptas tempore nam accusantium nisi earum eligendi quasi, asperiores, ipsa inventore aut rerum quibusdam quaerat dolorum sequi ex. Excepturi, tempora unde nihil eveniet rerum id!
+        Repellendus dolor ipsum esse ex iusto iste alias optio quas minus nisi nihil suscipit laboriosam, veniam, perferendis voluptates aut molestiae quidem exercitationem dolorum. Rem eius nostrum, et corrupti atque iusto!
+        Hic, maiores minima ipsum quae doloremque impedit. Ab natus esse porro voluptatum eius, enim accusamus quia ad culpa harum omnis debitis amet pariatur, architecto iusto sit totam? Quo, temporibus odit?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex suscipit cum quia aliquid dignissimos, soluta laboriosam itaque perspiciatis enim consectetur cupiditate ducimus sed eligendi, qui, ea nobis id laborum architecto.
+        Beatae amet itaque dolore provident reprehenderit. Modi alias dolorem corrupti cupiditate ea quae animi ratione nulla, velit exercitationem eius accusamus! Aut, vitae reprehenderit. Alias nulla perferendis, facilis accusantium aliquam id.
+        Reiciendis excepturi, officia beatae debitis accusantium illo explicabo qui pariatur culpa. Voluptas cumque consectetur quod ullam repellat recusandae qui, explicabo error incidunt, aut porro deserunt? Nesciunt laboriosam ullam ratione quis?
+        Ad cum fuga laborum autem est voluptatem beatae incidunt alias quasi quis, quae obcaecati veritatis optio officia, deserunt harum unde quia ea totam quo atque. Porro suscipit deleniti accusantium libero!
+        Voluptates minima quaerat reiciendis exercitationem. Unde, hic. Neque alias fugiat saepe cupiditate provident, tempora aliquam aperiam omnis ad quasi reiciendis, in ullam nihil doloremque iure autem rerum a aliquid quia?
+        Saepe placeat optio modi dignissimos asperiores quisquam, natus ipsum consequuntur unde cupiditate minima? Unde nobis totam temporibus illum ullam necessitatibus et earum voluptatem quis, recusandae, odit saepe corrupti? Sunt, minus!
+        Optio dolorum, labore eaque molestiae odit cupiditate id, error nam quos quod ducimus eum, provident consectetur ipsam similique eveniet tempore cum non? Aspernatur ipsum ipsa ab officia ex sint veritatis?
+        Vel enim reprehenderit necessitatibus ex aut cumque ducimus doloribus illum, nulla, asperiores eum quasi reiciendis dolorum laboriosam aliquid laudantium nobis? Numquam optio quo amet officiis iste exercitationem porro explicabo distinctio!
+        Porro consequatur expedita delectus. Sunt magni quaerat nostrum unde praesentium odit explicabo enim amet! Maiores fugit vero quibusdam excepturi culpa inventore enim minus atque illo vel ea aliquid, nihil doloribus?
+        Nobis neque natus ducimus, voluptatibus excepturi animi! Impedit ullam deleniti officia vel eaque, voluptates tempora aspernatur doloremque similique consequuntur, nemo fuga optio ea aut aperiam sequi! Doloremque earum voluptatem assumenda?
+        Veniam non, natus libero cupiditate dolores quas nihil illum, quibusdam earum voluptas ipsa vitae ipsum laboriosam incidunt sequi ab nisi, hic facere dolorem est fugiat harum nemo aliquid! Modi, fugiat?
+        Totam, quis doloribus. Commodi asperiores ullam voluptate reiciendis ad possimus provident officiis temporibus id rerum rem, delectus, quae perspiciatis quia, magnam beatae dolorem. Doloremque accusantium, quibusdam ipsum assumenda qui nulla?
+        Tenetur a assumenda eos, facilis autem fugiat repellendus? Magnam dolore, eos reprehenderit maxime delectus necessitatibus illum saepe quo mollitia tenetur tempore et vitae nobis totam ullam, nostrum deserunt voluptates! Voluptatibus!
+        Nesciunt modi, non necessitatibus quis, accusamus veritatis est asperiores dolor quod illo reprehenderit. Temporibus ipsam id reprehenderit quaerat, quasi ducimus veritatis modi incidunt, consequatur adipisci debitis est pariatur maiores iste.
+        Deleniti quos blanditiis, excepturi aut, necessitatibus qui possimus distinctio iusto earum, temporibus animi. Aliquid suscipit dolorum eius, minima optio mollitia sed ex veniam! Nam illo itaque aperiam placeat corporis error!
+        Voluptatum magni pariatur tempora, sunt ipsa id ullam optio iste iusto explicabo tenetur. Quibusdam, commodi reprehenderit! Tempore nobis temporibus maxime eius quod recusandae, delectus numquam animi! Ratione laborum consectetur repellat!
+        Aut expedita, quos repudiandae illo molestias maiores. Sunt aperiam similique veritatis commodi, quisquam at quis impedit deleniti? Maxime, blanditiis tenetur ipsa, error sapiente repellat odit ut qui nam magnam at.
+        Dicta rerum corrupti atque impedit dolorum. Asperiores autem nam incidunt beatae ipsum harum labore voluptates soluta et consequuntur expedita ipsam ullam, provident, amet dolores ut. Enim voluptates dolorum officia dolore.
+        Quae deserunt magnam blanditiis, earum doloribus dicta quia expedita maiores delectus iusto ut quaerat tempora quas nam, assumenda cupiditate provident quisquam mollitia facilis maxime impedit aspernatur? Quibusdam voluptas libero animi!
+        Est at omnis facilis, tempora sed voluptates ut quod facere error eveniet laborum nostrum ratione fugit a, quam asperiores, perspiciatis temporibus quidem tenetur nisi nobis architecto? Quidem molestias nam adipisci.
+    </div>
+@endsection
