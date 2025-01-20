857eda78b67409ede0d81d97de5c047de437ef3d
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-01-20 11:05:30 -0500

    chore: commands for local

--- routes/console.php
+++ routes/console.php
@@ -11,10 +11,14 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(SendReservationReminders::class)->timezone('America/Lima')->at('07:00:00')->everyThirtyMinutes();
+//Schedule::command(SendReservationReminders::class)->timezone('America/Lima')->at('07:00:00')->everyThirtyMinutes();
 
-Schedule::command(ChangeReservationInProgress::class)->timezone('America/Lima')->at('08:00:00')->everyThirtyMinutes();
+//Schedule::command(ChangeReservationInProgress::class)->timezone('America/Lima')->at('08:00:00')->everyThirtyMinutes();
 
-Schedule::command(ChangeReservationFinished::class)->timezone('America/Lima')->at('09:00:00')->everyThirtyMinutes();
+//Schedule::command(ChangeReservationFinished::class)->timezone('America/Lima')->at('09:00:00')->everyThirtyMinutes();
 
-//Schedule::command(SendReservationReminders::class)->timezone('America/Lima')->everyMinute();
+Schedule::command(SendReservationReminders::class)->timezone('America/Lima')->everyThirtyMinutes();
+
+Schedule::command(ChangeReservationInProgress::class)->timezone('America/Lima')->everyThirtyMinutes();
+
+Schedule::command(ChangeReservationFinished::class)->timezone('America/Lima')->everyThirtyMinutes();
