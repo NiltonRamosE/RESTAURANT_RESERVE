0066bd99b08a8ab446af51727c6d80f6884642f9
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-01-20 17:24:42 -0500

    feat: twilio send sms confirmation reserve

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -6,10 +6,18 @@ use App\Http\Requests\reserva\StoreReservaRequest;
 use App\Mail\ReserveMailConfirmation;
 use App\Models\Mesa;
 use App\Models\Reserva;
+use App\Services\TwilioService;
 use Illuminate\Support\Facades\Mail;
 
 class ReservaController extends Controller
 {
+
+    protected $twilio;
+
+    public function __construct(TwilioService $twilio)
+    {
+        $this->twilio = $twilio;
+    }
 
     public function index()
     {
@@ -32,15 +40,28 @@ class ReservaController extends Controller
             return redirect()->back()->withErrors(['error' => 'El usuario no tiene un cliente asociado.']);
         }
 
-        $validated['cliente_id'] = $userInSession['user']['id'];
+        $informationOfClient = $userInSession['user'];
+
+        $validated['cliente_id'] = $informationOfClient->id;
 
         $reservaCreated = Reserva::create($validated);
 
-        $informationOfClient = $userInSession['user'];
+        
 
         $mesaFound = Mesa::find($validated['mesa_id']);
 
         $this->sendReserveMailConfirmation($userInSession['correo'], $informationOfClient, $reservaCreated, $mesaFound);
+
+        $cliente_fullname = $informationOfClient->nombre . ' ' . $informationOfClient->apellido_paterno . ' ' . $informationOfClient->apellido_materno;
+
+        $message_sms = 
+            "
+                Estimado {$cliente_fullname}. Le agradecemos su preferencia al reservar en 7 SOPAS. La mesa Nº {$mesaFound->numero} fue reservada correctamente para el {$reservaCreated->fecha} a las {$reservaCreated->hora}. 
+                Con una duración {$reservaCreated->duracion} de {$reservaCreated->calculateDuration($reservaCreated->duracion)} horas. Para más información revisar su correo gmail. Gracias.
+                Atte. 7 SOPAS
+            ";
+
+        $this->twilio->sendSMS('+51' . $informationOfClient->celular, $message_sms);
 
         if (isset($reservaCreated)) {
             return to_route('reserva.index')->with('status', 'Reserva completada correctamente');
@@ -71,6 +92,6 @@ class ReservaController extends Controller
 
     private function sendReserveMailConfirmation($email_destinario, $informationOfClient, $reservaCreated, $mesaFound)
     {
-        //Mail::to($email_destinario)->send(new ReserveMailConfirmation($informationOfClient, $reservaCreated, $mesaFound));
+        Mail::to($email_destinario)->send(new ReserveMailConfirmation($informationOfClient, $reservaCreated, $mesaFound));
     }
 }
